c8d4f40d57b04bd9e188462c5b3751c4ccf39b4f
Eduardo Moreno <emoreno@uchicago.edu>
2015-07-13 15:34:38 -0700

    includes views skeleton and beginning of routes implentation

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -1,2 +1,26 @@
 class PuppiesController < ApplicationController
+	def index
+
+		@puppies = Puppies.all
+		
+	end
+	def create
+		
+	end
+	def new
+		
+	end
+	def edit
+		
+	end
+	def update
+		
+	end
+	def delete
+		
+	end
+	def show
+		
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root 'puppies#index'
   resources :puppies
 
   # The priority is based upon order of creation: first created -> highest priority.
