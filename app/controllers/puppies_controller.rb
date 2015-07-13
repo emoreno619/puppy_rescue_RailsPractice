583c3c4747fbfc31dd635ba7a7b179978bb37653
Eduardo Moreno <emoreno@uchicago.edu>
2015-07-13 16:04:03 -0700

    includes validations and partials for views

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -4,23 +4,34 @@ class PuppiesController < ApplicationController
 		@puppies = Puppy.all
 		
 	end
+
 	def create
-		
+		@puppy = Puppy.create(puppy_params)
 	end
+
 	def new
 		@puppy = Puppy.new
 	end
+
 	def edit
 		
 	end
+
 	def update
 		
 	end
+
 	def delete
 		
 	end
+
 	def show
 		
 	end
+
+	private
+		def puppy_params
+			
+		end
 
 end
--- app/models/puppy.rb
+++ app/models/puppy.rb
@@ -1,2 +1,6 @@
 class Puppy < ActiveRecord::Base
+
+	validates :name, uniqueness: true
+	validates :name, :bio, :age, :breed, :image, presence: true
+
 end
